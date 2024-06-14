ca8ee2586c3bb938190da6b0fcdcd1398511c0e9
77f315 <77f315@gmail.com>
2024-06-14 22:30:05 +0300

    added question data

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -1,44 +1,34 @@
 import React from "react";
 import './css/Test.css';
-
+import questions from "./data";
 function Test() {
     return (
         <>
-            <nav className="topbar">
-                {}
-            </nav>
-            <nav className="navbar">
-                {}
-            </nav>
-            <main>
-                <h1>Test 1</h1>
-                <div className="questions">
-                    <div className="question"> 
-                        <h2>1. Millised dokumendid peavad autojuhil kaasas olema kontrollijale esitamiseks, kui ei sõideta ainult Eesti piires?</h2>
-                        <form>
-                            <input type="radio" id="option-a" name="answer" value="a" />
-                            <label htmlFor="option-a">a. Tervisetõend</label><br />
-                            <input type="radio" id="option-b" name="answer" value="b" />
-                            <label htmlFor="option-b">b. Juhiluba või muu juhtimisõigust tõendav dokument</label><br />
-                        </form>
-                    </div>
-                    <div className="question"> 2</div>
-                    <div className="question"> 3</div>
-                    <div className="question"> 4</div>
-                    <div className="question"> 5</div>
-                    <div className="question"> 6</div>
-                    <div className="question"> 7</div>
-                    <div className="question"> 8</div>
-                    <div className="question"> 9</div>
-                    <div className="question"> 10</div>
-                    <div className="question"> 11</div>
-                    <div className="question"> 12</div>
-                    <div className="question"> 13</div>
-                    <div className="question"> 14</div>
-                    <div className="question"> 15</div>
-                    <div className="question">Results</div>
+        <div className="topbar"><h1>Test {questions[0].testnr}</h1></div>
+        <div className="container">
+            <div className="sidebar"></div>
+            <div className="questions">
+                <div className="question"> 
+                    <h2>{questions[0].id}. {questions[0].question}</h2>
+                    <form>
+                        <input type="radio" id="option-a" name="answer" value="a" />
+                        <label htmlFor="option-a">{questions[0].options[0].text}</label><br />
+                        <input type="radio" id="option-b" name="answer" value="b" />
+                        <label htmlFor="option-b">{questions[0].options[1].text}</label><br />
+                    </form>
                 </div>
-            </main>
+                <div className="question"> 
+                    <h2>1. Millised dokumendid peavad autojuhil kaasas olema kontrollijale esitamiseks, kui ei sõideta ainult Eesti piires?</h2>
+                    <form>
+                        <input type="radio" id="option-a" name="answer" value="a" />
+                        <label htmlFor="option-a">a. Tervisetõend</label><br />
+                        <input type="radio" id="option-b" name="answer" value="b" />
+                        <label htmlFor="option-b">b. Juhiluba või muu juhtimisõigust tõendav dokument</label><br />
+                    </form>
+                </div>
+               
+            </div>
+        </div>
         </>
     );
 }
---
+++ client/src/components/data.js
@@ -0,0 +1,42 @@
+const questions = [
+    {
+        testnr: 1,
+        id: 1,
+        question: "Millised dokumendid peavad autojuhil kaasas olema kontrollijale esitamiseks, kui ei sõideta ainult Eesti piires?",
+        options: [
+            { id: 'a', text: "Tervisetõend" },
+            { id: 'b', text: "Juhiluba või muu juhtimisõigust tõendav dokument" }
+        ]
+    },
+    {
+        testnr: 1,
+        id: 2,
+        question: "Mida nimetatakse manöövriks",
+        options: [
+            { id: 'a', text: "Mistahes sõiduraja vahetust." },
+            { id: 'b', text: "Sirgjoonelist tagurdamist." }
+        ]
+    },
+    {
+        testnr: 1,
+        id: 3,
+        question: "Mida peab tegema juht, kui ristmiku ees on see märk",
+        image: "https://sildid-kleebised.ee/wp-content/uploads/2022/06/stop-liiklusmark.png",
+        options: [
+            { id: 'a', text: "Jääma seisma märgi ees." },
+            { id: 'b', text: "Jääma stoppjoone puudumisel seisma lõikuva sõidutee ääre ees." }
+        ]
+    },
+    {
+        testnr: 4,
+        id: 1,
+        question: "Mida tähendab märgi ja lisateatetahvli ühend",
+        image: "https://www.gosafe.ee/media/inten/product/erply.s3.amazonaws.com/3169-3169_5cbf70cb7df842.33545129_132_large.png",
+        image2: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHrDI4-yHkT-RrTg8JoZcR6gDPwINzt2ErcQ&s", 
+        options: [
+            { id: 'a', text: "Järgneva 400 m ulatuses on samaliigiliste teede ristmike ala." },
+            { id: 'b', text: "400 m kaugusel on samaliigiliste teedega ristmik." }
+        ]
+    },
+];
+export default questions;
