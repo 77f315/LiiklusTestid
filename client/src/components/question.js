1347593150845c1d4d35be25f546709db555a8e4
77f315 <77f315@gmail.com>
2024-06-15 22:46:39 +0300

    question design

--- client/src/components/question.js
+++ client/src/components/question.js
@@ -1,9 +1,21 @@
-import React from "react";
-
+import React, { useState } from "react";
+import './css/question.css'
+import questions from "./data";
 const Question = () => {
+    
     return (
         <div className="container">
             <h1>Test</h1>
+            <hr/>
+            <h2>QUESTION ?????</h2>
+            <ul>
+                <li>a choiche a</li>
+                <li>b choiche b</li>
+                <li>c choiche c</li>
+                <li>d choiche d</li>
+            </ul>
+            <button>Järgmine küsimus</button>
+            <div className="index">question 1 of 120</div>
         </div>
     )
 }
