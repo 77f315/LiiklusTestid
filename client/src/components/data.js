580adf3e087fec9c36552ee98f02158136f4996f
77f315 <77f315@gmail.com>
2024-06-16 22:54:48 +0300

    lets user choose between option and highlights the correct option when wrong one is selected

--- client/src/components/data.js
+++ client/src/components/data.js
@@ -3,6 +3,7 @@ const questions = [
         testnr: 1,
         id: 1,
         question: "Millised dokumendid peavad autojuhil kaasas olema kontrollijale esitamiseks, kui ei sõideta ainult Eesti piires?",
+        ans : 2,
         options: [
             { id: 'a', text: "Tervisetõend" },
             { id: 'b', text: "Juhiluba või muu juhtimisõigust tõendav dokument" }
@@ -11,6 +12,7 @@ const questions = [
     {
         testnr: 1,
         id: 2,
+        ans: 1,
         question: "Mida nimetatakse manöövriks",
         options: [
             { id: 'a', text: "Mistahes sõiduraja vahetust." },
@@ -20,6 +22,7 @@ const questions = [
     {
         testnr: 1,
         id: 3,
+        ans: 2,
         question: "Mida peab tegema juht, kui ristmiku ees on see märk",
         image: "https://sildid-kleebised.ee/wp-content/uploads/2022/06/stop-liiklusmark.png",
         options: [
@@ -31,6 +34,7 @@ const questions = [
     {
         testnr: 1,
         id: 4,
+        ans: 2,
         question: "Mida tähendab märgi ja lisateatetahvli ühend",
         image: "https://www.gosafe.ee/media/inten/product/erply.s3.amazonaws.com/3169-3169_5cbf70cb7df842.33545129_132_large.png",
         image2: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHrDI4-yHkT-RrTg8JoZcR6gDPwINzt2ErcQ&s", 
--- client/src/components/question.js
+++ client/src/components/question.js
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import './css/question.css'
 import questions from "./data";
 
 const Question = () => {
     let [index , setIndex] = useState(0);
-    
+    let [lock, setLock] = useState(false);
+
+    let Option1 = useRef(null);
+    let Option2 = useRef(null);
+    let Option3 = useRef(null);
+    let Option4 = useRef(null);
+
+    let option_array = [Option1, Option2, Option3, Option4];
+    const checkAns = (e, ans) => {
+        if (lock === false){
+        if (question.ans === ans) {
+            e.target.classList.add("correct");
+            setLock(true);
+        } else{
+            e.target.classList.add("wrong")
+            setLock(true);
+            option_array[question.ans -1].current.classList.add("correct");
+        }
+    }
+    }
     const nextQuestion = () => {
         if (index < questions.length - 1){
             setIndex(index + 1);}
@@ -21,10 +40,28 @@ const Question = () => {
             <hr/>
             <h2>{question.id}. {question.question}</h2>
             <ul>
-                <li>{question.options[0].text}</li>
-                <li>{question.options[1].text}</li>
-                {question.options[2] &&<li>{question.options[2].text}</li>}
-                {question.options[3] && <li>{question.options[3].text}</li>}
+            <ul>
+                {question.options[0] && (
+                    <li ref={Option1} onClick={(e) => { checkAns(e, 1); }}>
+                        {question.options[0].text}
+                    </li>
+                )}
+                {question.options[1] && (
+                    <li ref={Option2} onClick={(e) => { checkAns(e, 2); }}>
+                        {question.options[1].text}
+                    </li>
+                )}
+                {question.options[2] && (
+                    <li ref={Option3} onClick={(e) => { checkAns(e, 3); }}>
+                        {question.options[2].text}
+                    </li>
+                )}
+                {question.options[3] && (
+                    <li ref={Option4} onClick={(e) => { checkAns(e, 4); }}>
+                        {question.options[3].text}
+                    </li>
+                )}
+            </ul>
 
         
             </ul>
