4ca4b2c1a2310f0d239314f05f726940531843e6
77f315 <77f315@gmail.com>
2024-06-16 22:38:44 +0300

    display data from database, and made next and previous question buttons functional

--- client/src/components/question.js
+++ client/src/components/question.js
@@ -1,21 +1,38 @@
 import React, { useState } from "react";
 import './css/question.css'
 import questions from "./data";
+
 const Question = () => {
+    let [index , setIndex] = useState(0);
     
+    const nextQuestion = () => {
+        if (index < questions.length - 1){
+            setIndex(index + 1);}
+    }
+    const previousQuestion = () => {
+        if (index > 0) {
+            setIndex(index - 1)
+        }
+    }
+    const question = questions[index];
     return (
         <div className="container">
             <h1>Test</h1>
             <hr/>
-            <h2>QUESTION ?????</h2>
+            <h2>{question.id}. {question.question}</h2>
             <ul>
-                <li>a choiche a</li>
-                <li>b choiche b</li>
-                <li>c choiche c</li>
-                <li>d choiche d</li>
+                <li>{question.options[0].text}</li>
+                <li>{question.options[1].text}</li>
+                {question.options[2] &&<li>{question.options[2].text}</li>}
+                {question.options[3] && <li>{question.options[3].text}</li>}
+
+        
             </ul>
-            <button>Järgmine küsimus</button>
-            <div className="index">question 1 of 120</div>
+            <div className="question-container">
+                <button onClick={previousQuestion}>eelmine küsimus</button>
+                <button onClick={nextQuestion}>Järgmine küsimus</button>
+            </div>
+            <div className="index">question {question.id} of {questions.length}</div>
         </div>
     )
 }
