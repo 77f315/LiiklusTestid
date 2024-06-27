d974b59fadf5fc491e9b3e61a3bf3fdad8c3f226
77f315 <77f315@gmail.com>
2024-06-27 17:21:43 +0300

    added a way back from the results page

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import {useNavigate } from 'react-router-dom';
 const Results = () => {
+    const navigate = useNavigate();
     const correct = window.localStorage.getItem('correct');
     const wrong = window.localStorage.getItem('wrong');
     return (
         <div>
-            <h1>{Number(correct) + 1 } out of {Number(correct) + 1 +  Number(wrong)}</h1>
+            <h1>{Number(correct) + 1 } out of {Number(correct) +Number(wrong) + 1}</h1>
+            <button onClick={() => navigate("/")}>again</button>
         </div>
     );
 }
